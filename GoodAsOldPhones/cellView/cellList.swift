63363880d99639d36e91324f2df68e367559e57e
UCDAyoung <qkr93643857@gmail.com>
2021-08-18 01:21:31 +0900

    2주차 GoodAsOldPhones를 시도했다. Navigation 컨트롤러를 사용하는 것이었다. 일단은 꽤 부딪혀보면서 만들어서 얻는 것들이 꽤 있었다. 하지만, 내가 간과했던 부분은 왜 switch로만 만들 생각을 했을까이다... 분명 더 좋은 방법이 있을텐데.. 이 부분을 항상 유념하자.

---
+++ GoodAsOldPhones/cellView/cellList.swift
@@ -0,0 +1,14 @@
+//
+//  firstView.swift
+//  GoodAsOldPhones
+//
+//  Created by 박준영 on 2021/08/17.
+//
+
+import UIKit
+
+class cellView: UITableViewCell {
+    @IBOutlet weak var productsImg: UIImageView!
+    @IBOutlet weak var productsLabel: UILabel!
+    
+}
